7d5ff25394eeb507c134286d1a2ce91a78df5534
Akio Suzuki <116661015+Kio-Suzuki@users.noreply.github.com>
2022-11-12 20:11:24 -0300

    Create final.c

---
+++ final.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+// STRUCTS
+
+
+typedef struct pessoas{
+    int cpf, ddd, numero, codigo=numeroRandom();
+    char nome[50], sobrenome[50];
+    
+    
+}pes;
+
+struct pontes{
+    int cpf, ddd, numero, codigo=numeroRandom();
+    char nome[50], sobrenome[50];
+};
+
+struct restaurant{
+    int cep;
+    char nome[50];
+    endereço
+};
+
+struct ciclos{
+    int 
+    struct restaurant;
+};
+
+struct parceiros{
+    int nome;
+    
+};
+
+
+//FUNÇÕES (PROTOTIPOS)
+
+
+void numeroRandom(){
+
+};
+
+void cadastroRest(){
+    struct restaurant;
+    codigoRandomUnico();
+    sobras(kg)
+};
+
+void cadastroPonte(){
+    codigoRandomUnico();
+        struct pontes;
+
+};
+
+void cadastroIndividuo(){
+    codigoRandomUnico();
+    struct pessoas();
+};
+
+void cadastroParceiros(){
+    codigoRandomUnico();
+    struct parceiros;
+};
+
+void pesquisar(){
+
+};;
+
+
+// MAIN
+
+int main(void){
+
+}
+
+
+
+// FUNÇÕES (CONSTRUIDAS)
+
+void numeroRandom(){
+
+};
+
+void cadastroRest(){
+    struct restaurant;
+    codigoRandomUnico();
+    sobras(kg)
+};
+
+void cadastroPonte(){
+    codigoRandomUnico();
+        struct pontes;
+
+};
+
+void cadastroIndividuo(){
+    codigoRandomUnico();
+    struct pessoas();
+};
+
+void cadastroParceiros(){
+    codigoRandomUnico();
+    struct parceiros;
+};
+
+void pesquisar(){
+
+};;
