2e338685ffb23c469721c44225cd02d6e0d32c15
pedrohcmoda <phcunha47@gmail.com>
2022-11-13 03:38:38 -0300

    Update final.c

--- final.c
+++ final.c
@@ -1,80 +1,68 @@
+  
+
+// talvez oq coloquei ai não faça o menor sentioo
+
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-//testando github test
-
-// STRUCTS
-
-
-typedef struct pessoas{
-    int cpf, ddd, numero, codigo=numeroRandom();
-    char nome[50], sobrenome[50];
-    
-    
-}pes;
-
-struct pontes{
-    int cpf, ddd, numero, codigo=numeroRandom();
-    char nome[50], sobrenome[50];
-};
-
-struct restaurant{
-    int cep;
-    char nome[50];
-    endereço
-};
-
-struct ciclos{
-    int 
-    struct restaurant;
-};
-
-struct parceiros{
-    int nome;
-    
-};
 
 
 //FUNÇÕES (PROTOTIPOS)
 
 
-void numeroRandom(){
+int numeroRandom();
+void cadastroRest();
+void cadastroPonte();
+pes cadastroIndividuo();
+void cadastroParceiros();
+void pesquisar();
+void deleteRegistro();
 
-};
+// STRUCTS
 
-void cadastroRest(){
+
+typedef struct pessoas{
+    int cpf, ddd, numero, codigo;
+    char nome[50], sobrenome[50];
+}pes;
+
+typedef struct pontes{
+    int cpf, ddd, numero, codigo;
+    char nome[50], sobrenome[50];
+}pont;
+
+typedef struct restaurant{
+    int cep;
+    char nome[50], sobras;
+}restt;
+
+typedef struct ciclos{
+    int kg, dia, mes, ano;
     struct restaurant;
-    codigoRandomUnico();
-    sobras(kg)
-};
+    
+}cic;
 
-void cadastroPonte(){
-    codigoRandomUnico();
-        struct pontes;
+typedef struct parceiros{
+    int nome;
+    
+}parc;
 
-};
 
-void cadastroIndividuo(){
-    codigoRandomUnico();
-    struct pessoas();
-};
-
-void cadastroParceiros(){
-    codigoRandomUnico();
-    struct parceiros;
-};
-
-void pesquisar(){
-
-};;
 
 
 // MAIN
 
 int main(void){
-
+    pes *pessoa=malloc(10000*sizeof(pes));
+    pont *ponte=malloc(10000*sizeof(pont));
+    restt *rest=malloc(10000*sizeof(restt));
+    cic *ciclos=malloc(10000*sizeof(cic));
+    parc *parceiros=malloc(10000*sizeof(parc));
+    int numPes, numPont, numRest, numCic, numParc;
+    numPes=1
+    cadastroIndividuo(&pessoa[numPes]);
 }
 
 
@@ -82,7 +70,9 @@ int main(void){
 // FUNÇÕES (CONSTRUIDAS)
 
 void numeroRandom(){
-
+    srand(time(NULL));
+    int valor = rand()%10000;
+    return valor;
 };
 
 void cadastroRest(){
@@ -97,11 +87,12 @@ void cadastroPonte(){
 
 };
 
-void cadastroIndividuo(){
-    codigoRandomUnico();
-    struct pessoas();
+void cadastroIndividuo(pes *pessoa, int pos){
+    pessoa[pos].codigo=numeroRandom();
+    fgets(pessoa[pos].nome,50,stdin);
+    fgets(pessoa[pos].sobrenome,50,stdin);
+    scanf("%d", pessoa[pos].cpf);
 };
-
 void cadastroParceiros(){
     codigoRandomUnico();
     struct parceiros;
@@ -109,4 +100,5 @@ void cadastroParceiros(){
 
 void pesquisar(){
 
-};;
+};
+
